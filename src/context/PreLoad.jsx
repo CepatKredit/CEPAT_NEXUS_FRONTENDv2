5ac946905d577c49a59c3c7dbcb556786ec7aca6
Rex Egonio Jr. <rex.egonio@cepatkredit.com>
2024-11-07 09:09:41 +0800

    Fix
    -Counting of Status in Tiles

--- src/context/PreLoad.jsx
+++ src/context/PreLoad.jsx
@@ -52,6 +52,7 @@ function PreLoad({ children }) {
     React.useEffect(() => { getDisbursementList.refetch(); SET_REFRESH_LAN(0); }, [GET_REFRESH_LAN])
 
     const [GET_DATA_COUNTER, setDataCounter] = React.useState([])
+    const [getRefreshTileCounter, setRefreshTileCounter] = React.useState(0)
     const TileCountListQuery = useQuery({
         queryKey: ['TileCountListQuery'],
         queryFn: async () => {
@@ -64,6 +65,9 @@ function PreLoad({ children }) {
         retryDelay: 1000,
         staleTime: 5 * 1000
     });
+    function SET_REFRESH_TILE_COUNTER(value) { setRefreshTileCounter(value) }
+
+    React.useEffect(() => { TileCountListQuery.refetch(); SET_REFRESH_TILE_COUNTER(0) }, [getRefreshTileCounter])
 
     //REFETCH FOR PRELOAD DATA
     React.useEffect(() => {
@@ -71,42 +75,6 @@ function PreLoad({ children }) {
         getPurposeList.refetch()
         TileCountListQuery.refetch()
     }, [])
-
-    function SET_PATH_LOCATION(SELECTED_STATUS) {
-        if (SELECTED_STATUS === 'DECLINED') { localStorage.setItem('SP', '/ckfi/declined'); }
-        else if (SELECTED_STATUS === 'CANCELLED') { localStorage.setItem('SP', '/ckfi/cancelled'); }
-
-        else if (SELECTED_STATUS === 'LACK OF DOCUMENTS') { localStorage.setItem('SP', '/ckfi/lack-of-documents'); }
-        else if (SELECTED_STATUS === 'FOR CREDIT ASSESSEMENT') { localStorage.setItem('SP', '/ckfi/credit-assessment-list'); }
-        else if (SELECTED_STATUS === 'FOR INITIAL INTERVIEW') { localStorage.setItem('SP', '/ckfi/initial-interview'); }
-        else if (SELECTED_STATUS === 'FOR WALK-IN') { localStorage.setItem('SP', '/ckfi/walk-in'); }
-        else if (SELECTED_STATUS === 'COMPLIED - LACK OF DOCUMENTS') { localStorage.setItem('SP', '/ckfi/complied/lack-of-documents'); }
-        else if (SELECTED_STATUS === 'RECEIVED') { localStorage.setItem('SP', '/ckfi/received'); }
-        else if (SELECTED_STATUS === 'REASSESSED TO CREDIT ASSOCIATE') { localStorage.setItem('SP', '/ckfi/return/credit-associate'); }
-        else if (SELECTED_STATUS === 'RETURN TO CREDIT OFFICER') { localStorage.setItem('SP', '/ckfi/return/credit-officer'); }
-        else if (SELECTED_STATUS === 'ON WAIVER') { localStorage.setItem('SP', '/ckfi/on-waiver'); }
-
-        else if (SELECTED_STATUS === 'SCREENING') { localStorage.setItem('SP', '/ckfi/queue-bucket'); }
-        else if (SELECTED_STATUS === 'INTERVIEW') { localStorage.setItem('SP', '/ckfi/queue-bucket'); }
-        else if (SELECTED_STATUS === 'FOR CALLBACK') { localStorage.setItem('SP', '/ckfi/queue-bucket'); }
-        else if (SELECTED_STATUS === 'FOR VERIFICATION') { localStorage.setItem('SP', '/ckfi/for-verification'); }
-        else if (SELECTED_STATUS === 'PRE-CHECK') { localStorage.setItem('SP', '/ckfi/pre-check'); }
-        else if (SELECTED_STATUS === 'FOR APPROVAL') { localStorage.setItem('SP', '/ckfi/for-approval'); }
-        else if (SELECTED_STATUS === 'REASSESSED TO MARKETING') { localStorage.setItem('SP', '/ckfi/reassessed/marketing'); }
-        else if (SELECTED_STATUS === 'PRE-APPROVAL') { localStorage.setItem('SP', '/ckfi/reassessed/credit-officer'); }
-        else if (SELECTED_STATUS === 'APPROVED (TRANS-OUT)') { localStorage.setItem('SP', '/ckfi/trans-in'); }
-
-        else if (SELECTED_STATUS === 'RETURN TO LOAN PROCESSOR') { localStorage.setItem('SP', '/ckfi/return/loan-processor'); }
-        else if (SELECTED_STATUS === 'FOR DOCUSIGN') { localStorage.setItem('SP', '/ckfi/for-docusign'); }
-        else if (SELECTED_STATUS === 'OK FOR DOCUSIGN') { localStorage.setItem('SP', '/ckfi/ok/for-docusign'); }
-        else if (SELECTED_STATUS === 'TAGGED FOR RELEASE') { localStorage.setItem('SP', '/ckfi/tagged-for-release'); }
-        else if (SELECTED_STATUS === 'FOR DISBURSMENT') { localStorage.setItem('SP', '/ckfi/for-disbursement'); }
-
-        else if (SELECTED_STATUS === 'RELEASED') { localStorage.setItem('SP', '/ckfi/released'); }
-        else if (SELECTED_STATUS === 'CONFIRMATION') { localStorage.setItem('SP', '/ckfi/confirmation'); }
-        else if (SELECTED_STATUS === 'CONFIRMED') { localStorage.setItem('SP', '/ckfi/confirmed'); }
-        else { localStorage.setItem('SP', '/ckfi/undecided'); }
-    }
 
     return (
         <DataContext.Provider value={{
@@ -116,8 +84,8 @@ function PreLoad({ children }) {
             SET_LOAN_APPLICATION_NUMBER,
             GET_TOTAL_AMOUNT,
             SET_REFRESH_LAN,
-            SET_PATH_LOCATION,
-            GET_DATA_COUNTER
+            GET_DATA_COUNTER,
+            SET_REFRESH_TILE_COUNTER
         }}>
             {children}
         </DataContext.Provider>
