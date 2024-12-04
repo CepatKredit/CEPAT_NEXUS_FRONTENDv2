adb7036b470164ec07d58245ec08a56accea5cff
rhafaeloconer-prog <rafael.oconer@cepatkredit.com>
2024-12-04 15:08:16 +0800

    Fix
    -Fix Position of button for hide Dashmini

--- src/layouts/SideNav.jsx
+++ src/layouts/SideNav.jsx
@@ -276,7 +276,7 @@ function SideNav() {
                     <Button
                       shape="circle"
                       onClick={toggleVisibility}
-                      className={`ml-[48rem] fixed left-1/2 transform -translate-x-1/2 transition-all duration-300 ${isVisible ? "top-[11rem]" : "top-[3.7rem]"} z-10 bg-transparent border-none hover:bg-transparent`}
+                      className={`ml-[6rem] fixed left-1/2 transform -translate-x-1/2 transition-all duration-300 ${isVisible ? "top-[11rem]" : "top-[3.7rem]"} z-10 bg-transparent border-none hover:bg-transparent`}
                       icon={isVisible ? <CaretUpOutlined /> : <CaretDownOutlined />}
                     />
 
