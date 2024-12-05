b946c15afcaa656a601781b795ff6e65c5258a65
rhafaeloconer-prog <rafael.oconer@cepatkredit.com>
2024-12-05 10:52:04 +0800

    Edit
    - adjustment sa logic ng checkbox for Beneficiary dependents table

--- src/containers/dataList/TabList/beneficiaryDetails/EditBeneficiaryDetails.jsx
+++ src/containers/dataList/TabList/beneficiaryDetails/EditBeneficiaryDetails.jsx
@@ -750,7 +750,7 @@ function EditBeneficiaryDetails({ data, receive, presaddress, BorrowerId, Sepcob
                     />
                 )}
                 {/* dito ibabato ang check galing context upang gamitin sa condition na pag display ng relativetavle na once 1 ang makuha sa context ididisplay nya ito realtime then pag naman 0 hindi */}
-                {getAppDetails?.MarriedPBCB && (User !== 'LC' && (
+                {!getAppDetails?.MarriedPBCB && (User !== 'LC' && (
                     <div className="w-full mt-[2rem] mx-auto">
                         <RelativesTable BorrowerId={BorrowerId} onUpdateCount={(count) => setRelativesCount(count)} data={data} isOfw={2} />
                     </div>
